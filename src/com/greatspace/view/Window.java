692a8827b2342aa1396e823293e1df250086d80a
陳泰元 <@Oscar-DESKTOP>
2018-12-15 00:36:29 +0800

    完成Window class初步建置

--- src/com/greatspace/view/Window.java
+++ src/com/greatspace/view/Window.java
@@ -1,9 +1,12 @@
-/**
- * @author: Derick Felix
- * @Data: 02/13/2016
- * @Release: 2.1
+/**\mainpage   捲軸射擊小遊戲  專題
+ *  - \subpage  @author:110616038 陳泰元
+ *  - \subpage @改寫自author: Derick Felix
+ *  - \subpage @Release: 1.0
+ *  - \subpage @Data: 12/14/2018
+ * 
  * @Class: Window
- * @Objective: Create the Main Window
+ * @Objective: 創建Window物件
+ * 
  */
 
 package com.greatspace.view;
@@ -20,39 +23,41 @@ import javax.swing.JOptionPane;
 
 public class Window {
 
-	public static JFrame frame= new JFrame("宇宙射擊");
-    public Window() {
+	public static JFrame frame= new JFrame("宇宙射擊"); //遊戲主畫面框架
+	
+	/**
+	 * @brief 創建新遊戲的首要步驟
+	 */
+    public Window() { 
 
-    	
-        
-            Game f = new Game();
+            Game gameCore = new Game(); //建構遊戲核心
          							
-            f.checkPlayer();//定People number
-        	//frame = new JFrame("宇宙射擊");
-            frame.add(f);
-            frame.setJMenuBar(f.criarMenu());
-            frame.setDefaultCloseOperation(3); //3:The exit application default window close operation
+            gameCore.checkPlayer();//呼叫函式確認玩家人數
+            frame.add(gameCore);
+            frame.setJMenuBar(gameCore.createMenu());
+            frame.setDefaultCloseOperation(3); //3:按下關閉按鍵視窗就執行System.exit(0)的指令
             frame.setSize(500, 420);
-            frame.setLocationRelativeTo(null); //the window is placed in the center of the screen
-            frame.setResizable(false); //Can't control the size of frame
+            frame.setLocationRelativeTo(null); //將視窗的位置擺向螢幕中央
+            frame.setResizable(false); //不允許使用者控制視窗縮放功能
             try {
-                URL sourceImage = Window.class.getResource("../sprites/gsicon.png"); //寫路徑
-                BufferedImage image = ImageIO.read(sourceImage); //載icon圖檔
-                frame.setIconImage(image); //定 frame icon
+                URL sourceImage = Window.class.getResource("../sprites/gsicon.png"); //取得圖片路徑物件
+                BufferedImage image = ImageIO.read(sourceImage); //載入icon圖檔
+                frame.setIconImage(image); //設定視窗icon圖示
             } catch (IOException e) {
-                System.out.println("圖片錯誤: " + e);
+                System.out.println("圖片錯誤: " + e); //萬一找不到時回報錯誤資訊
             }
             frame.setVisible(true);
-        
-        
 
-        
     }
-
+    
+    /**
+	 * @brief 此處為程式開始點
+	 * @param [in] String[] args
+	 */
     public static void main(String[] args) {
        
         try {
-            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());//取得符合裝置平台的佈景樣式
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             
